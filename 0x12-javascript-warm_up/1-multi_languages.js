89321150aa179288eb3134e7b788eef4e5b5df74
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-04-09 00:42:24 +0200

    update 6

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -5,7 +5,3 @@ const myVar3 = 'JavaScript is amazing';
 console.log(myVar1);
 console.log(myVar2);
 console.log(myVar3);
-<<<<<<< HEAD
-
-=======
->>>>>>> 27c72891a6d29a468481e445effa3eb02f572b1e
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,20 +1,10 @@
 #!/usr/bin/node
-<<<<<<< HEAD
+
 // Prints a message depending on number of arguments passed
 args = process.argv;
 if (args.length === 2) {
   console.log("No argument");
 } else if (args.length === 3) {
-=======
-//  Prints a message depending on number of arguments passed
 
-const args = process.argv;
-if (process.argv === 2) {
-  console.log("No argument");
-} else if (process.argv === 3) {
->>>>>>> 27c72891a6d29a468481e445effa3eb02f572b1e
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
 }
 
