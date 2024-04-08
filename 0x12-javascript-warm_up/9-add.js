58ee305a0a60993453b584bbe6baf8556d991086
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-04-09 00:45:10 +0200

    ubdate7

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // prints the addition of 2 integers
 
-function add(a, b) {
- console.log("The sum of", a, "and", b, "is:", a + b);
-}
-
-// Example usage:
-add(5, 10); // Output: The sum of 5 and 10 is: 15
+function add (a, b) {
+    return parseInt(a) + parseInt(b);
+  }
+  
+  console.log(add(process.argv[2], process.argv[3]));
