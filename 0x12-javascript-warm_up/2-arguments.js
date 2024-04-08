8b44c98e61db7454d6f364023100f2a4ee4fafea
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-04-08 23:00:52 +0200

    ubdate2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-//  Prints a message depending on number of arguments passed
-const args = process.argv;
-if (process.argv === 2) {
-  console.log("No argument");
-} else if (process.argv === 3) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-=======
 // Prints a message depending on number of arguments passed
 args = process.argv;
 if (args.length === 2) {
-	  console.log('No argument');
+  console.log("No argument");
 } else if (args.length === 3) {
-	  console.log('Argument found');
+  console.log("Argument found");
 } else {
-	  console.log('Arguments found');
->>>>>>> 09620b1680f2742e71cf4a01bcf47e526a4559ce
+  console.log("Arguments found");
 }
