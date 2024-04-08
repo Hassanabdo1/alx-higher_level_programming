8b26749e47ad99914d2b5691db9289088673f3e9
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-04-08 19:23:58 +0200

    ubdate2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+//  Prints a message depending on number of arguments passed
+const args = process.argv
 if (process.argv === 2) {
-	  console.log("No argument");
+	console.log("No argument");
 } else if (process.argv === 3) {
-	  console.log("Argument found");
+	console.log("Argument found");
 } else {
-	  console.log("Arguments found");
+	 console.log("Arguments found");
 }
 
