b711e366e9965e6aa9a5c0b381f437cd6d03a37f
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-04-08 18:46:15 +0200

    ubdate1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-const myVar = "C is fun";
-const myVar1 = "Python is cool";
-const myVar2 = "JavaScript is amazing";
+let myVar = "C is fun";
+let myVar1 = "Python is cool";
+let myVar2 = "JavaScript is amazing";
 console.log(myVar);
 console.log(myVar1);
 console.log(myVar2);
-
-
-
-
-
-
